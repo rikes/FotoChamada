6e412503105a641470fe1e6fcaae09b11f7baa91
Henrique Santana <henricke.ss@gmail.com>
2017-06-26 01:45:48 -0300

    Inserindo entidades

---
+++ FotoChamada/Entidades/Disciplina.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FotoChamada.entidades
+{
+    class Disciplina
+    {
+        private Turma turma;
+        private List<String> listDisciplina;
+    }
+}
---
+++ FotoChamada/Entidades/Turma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FotoChamada.entidades
+{
+    class Turma
+    {
+        private Professor professor;
+        private Aluno aluno;
+        private String nome;
+        private char semestre;
+        private int ano;
+    }
+}
